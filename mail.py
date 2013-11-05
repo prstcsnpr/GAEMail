51d0425b24d576fc677a6e98c042992becd99ef2
prstcsnpr <prstcsnpr@10.2.78.18>
2013-11-05 15:11:11 +0800

    可以分布式扩容式发邮件

--- mail.py
+++ mail.py
@@ -5,46 +5,43 @@ import logging
 import string
 from google.appengine.api.labs import taskqueue
 from google.appengine.api import mail
+from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 
 class MailHandler(webapp.RequestHandler):
     def post(self):
-        to = self.request.get("to")
-        subject = self.request.get("subject")
-        body = self.request.get("body")
-        html = self.request.get("html")
-        mail.send_mail(sender="prstcsnpr@gmail.com", to=to, subject=subject, body=body, html=html)
-        logging.info('Mail to %s' % (to))
+        client = self.request.get("client")
+        formula = self.request.get("formula")
+        if 'magicformula' == formula:
+            magic = self._get_magic_formula_result()
+            mail.send_mail(sender="prstcsnpr@gmail.com", to=client, subject='神奇公式', body='', html=magic)
+            logging.info('Mail to %s for MagicFormula' % (client))
+        if 'grahamformula' == formula:
+            graham = self._get_graham_formula_result()
+            mail.send_mail(sender="prstcsnpr@gmail.com", to=client, subject='格雷厄姆公式', body='', html=graham)
+            logging.info('Mail to %s for GrahamFormula' % (client))
         
+    def _get_magic_formula_result(self):
+        result = urlfetch.fetch(url='http://qmagicformula.appspot.com/magicformula')
+        if result.status_code == 200:
+            return result.content
+        
+    def _get_graham_formula_result(self):
+        result = urlfetch.fetch(url='http://qmagicformula.appspot.com/grahamformula')
+        if result.status_code == 200:
+            return result.content
 
 class PostOfficeHandler(webapp.RequestHandler):
     def post(self):
-        subject = self.request.get("subject")
-        content = self.request.get("content")
-        postmen = []
-        with open('postmen') as file:
-            for line in file.readlines():
-                postman = line.split()[0]
-                postmen.append(postman)
-        i = 0;
-        with open('clients') as file:
-            for line in file.readlines():
-                client = line.split()[0]
-                postman = postmen[i % len(postmen)];
-                taskqueue.add(url=postman,
-                              queue_name="mail",
-                              method="POST",
-                              params={'to' : client,
-                                      'subject' : subject,
-                                      'body' : '',
-                                      'html' : content,
-                                      })
-                logging.info('Mail to %s by %s' % (client, postman))
-                i = i + 1
+        client = self.request.get("client")
+        formula = self.request.get("formula")
+        taskqueue.add(url='/tasks/mail',
+                      queue_name='mail',
+                      params={'client' : client, 'formula': formula},
+                      method='POST')
         
-
 
 application = webapp.WSGIApplication([('/tasks/mail', MailHandler),
                                       ('/tasks/postoffice', PostOfficeHandler)], 
